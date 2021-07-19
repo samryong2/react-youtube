34f7d2cf0a624d3484ebde76deceacfa287f3d3e
Dev-hanseo <85448618+Dev-hanseo@users.noreply.github.com>
2021-07-19 23:59:10 +0900

    리스트 아이템 완성

--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Video_item from '../video_item/video_item';
+import styles from './video_list.module.css'
 
 const VideoList = (props) => (
-        <ul>
+        <ul className={styles.videos}>
             {props.videos.map(video => <Video_item key={video.id} video={video} />)}
         </ul>
     );
