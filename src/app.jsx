714ba19d7d623b6490593c4818313a449567b89e
Dev-hanseo <85448618+Dev-hanseo@users.noreply.github.com>
2021-07-24 23:46:39 +0900

    MVC 패턴 적용

--- src/app.jsx
+++ src/app.jsx
@@ -4,30 +4,17 @@ import SearchHeader from './components/search/search_header';
 import VideoList from './components/video_list/video_list';
 
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://www.googleapis.com/youtube/v3/search?key=AIzaSyCvA5YypBkHGWVDY1A7hF2IoytbDPRZTrY&part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCvA5YypBkHGWVDY1A7hF2IoytbDPRZTrY`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
   }
   useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCvA5YypBkHGWVDY1A7hF2IoytbDPRZTrY&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCvA5YypBkHGWVDY1A7hF2IoytbDPRZTrY", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
   },[]);
   return (
         <div className={styles.app}>
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 import Video_item from '../video_item/video_item';
 import styles from './video_list.module.css'
 
-const VideoList = (props) => (
+const VideoList = (props) => {
+
+    console.log(`props.videos :${props.videos}`);
+    console.log(`props.videos :${props.videos}`);
+
+    // {props.videos.map(video => <Video_item key={video.id} video={video} />)}
+
+    return  ( 
         <ul className={styles.videos}>
             {props.videos.map(video => <Video_item key={video.id} video={video} />)}
         </ul>
-    );
+    )};
 
 export default VideoList;
